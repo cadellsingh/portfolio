a9a2bc314f4050ae3e97291c6e4dc0658f20ece7
Cadell Singh <cadellsingh9@gmail.com>
2021-02-02 06:34:38 -0400

    added spotostats app

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,6 +5,7 @@ import AllInOne from "../images/all-in-one-sports.png";
 import DevJobs from "../images/devjobs.png";
 import Todo from "../images/todo.png";
 import NumberSystem from "../images/number-system-conversion.png";
+import SpotoStats from "../images/spotoStats.png"
 import EachProject from "./eachProject";
 import "aos/dist/aos.css";
 
@@ -31,40 +32,47 @@ const Projects = () => {
     <Container id="projects">
       <h2>projects</h2>
       <ProjectsContainer>
+      <EachProject
+          githubLink="https://github.com/cadellsingh/spotifyStats"
+          img={SpotoStats}
+          liveLink="https://spotostats.netlify.app/"
+          title="spotoStats"
+          direction="fade-right"
+        />
         <EachProject
           githubLink="https://github.com/cadellsingh/devJobs"
           img={DevJobs}
           liveLink="https://devjobss.netlify.app/"
           title="devjobs"
-          direction="fade-right"
+          direction="fade-left"
         />
         <EachProject
           githubLink="https://github.com/cadellsingh/converter"
           img={NumberSystem}
           liveLink="https://numbersystemconversion.netlify.app/"
           title="numberSystemConversion"
-          direction="fade-left"
+          direction="fade-right"
         />
         <EachProject
           githubLink="https://github.com/cadellsingh/All-in-one-sports-page"
           img={AllInOne}
           liveLink="https://all-in-one-sports.netlify.app/"
           title="allInOne"
-          direction="fade-right"
+          direction="fade-left"
         />
         <EachProject
           githubLink="https://github.com/cadellsingh/covid-19-tracker"
           img={CovidTracker}
           liveLink="https://cadellsingh-covid-19-tracker.netlify.app/"
           title="covidTracker"
-          direction="fade-left"
+          direction="fade-right"
         />
         <EachProject
           githubLink="https://github.com/cadellsingh/todoList"
           img={Todo}
           liveLink="https://cadellsingh-todolist.netlify.app/"
           title="todo"
-          direction="fade-right"
+          direction="fade-left"
         />
       </ProjectsContainer>
     </Container>
